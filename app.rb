e9e7441a013776eafe8fcf7035320cbfdc11bf09
Paul Kelly <paul.kelly20@gmail.com>
2018-07-01 18:13:04 +0100

    Altered app.rb

--- app.rb
+++ app.rb
@@ -1,9 +1,9 @@
 require( 'sinatra' )
-require_relative('controllers/artists_controller')
-require_relative('controllers/albums_controller')
-require_relative('controllers/genre_controller')
-require_relative('controllers/invertory_controller')
-require_relative('controllers/shop_controller')
+require_relative('./controllers/artists_controller.rb')
+require_relative('./controllers/albums_controller.rb')
+require_relative('./controllers/genre_controller.rb')
+require_relative('./controllers/invertory_controller.rb')
+require_relative('./controllers/shop_controller.rb')
 
 get "/" do
  erb (:index)
